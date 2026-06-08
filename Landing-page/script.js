d8c9d902555c185ddac3824528dd2f06cb85675a
Vivek Bhardwaj <vp0444199@gmail.com>
2026-06-08 09:18:00 +0530

    add hamburger for mobile devices

--- Landing-page/script.js
+++ Landing-page/script.js
@@ -3,3 +3,9 @@ const themeToggle = document.getElementById("theme-toggle");
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
